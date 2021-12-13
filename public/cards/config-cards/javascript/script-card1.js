81faeb05d67cb3718497dd9343b9ffb76605543b
Julianne Wolkmer Henrique Silva <trollhammer590@outlook.com>
2021-12-13 11:26:01 -0300

    deck system updated

---
+++ public/cards/config-cards/javascript/script-card1.js
@@ -0,0 +1,262 @@
+$(document).ready(function(){
+    $("#button-03").click(function(){
+        domtoimage.toBlob(document.getElementById("carta"))
+        .then(function(blob){
+            window.saveAs(blob, "carta.png");
+            document.getElementById("mat-01").style=
+            'display: block;'+
+            'margin: -175px 0 0 90px;'+
+            'color: green;';
+        });
+    });
+
+    $("#carta").css('border', '2px solid #000');                    /* card */
+    $("#imagem-carta-container").css('border', '2px solid #000');   /* img-card */
+    $("#custo-mana-carta").css('background-color', "#000");         /* mana */
+    $("#cor-texto-carta-input").val("#ffffff");                     /* card-collor */
+});
+
+/**/
+
+$("#nome-carta-input").on("input", function(){
+    $("#nome-carta").html($(this).val());
+});
+$("#custo-mana-carta-input").on("input", function(){
+    $("#custo-mana-carta").html($(this).val());
+});
+$("#imagem-url-carta-input").on("input", function(){
+    $("#img-carta").attr("src", $(this).val());
+});
+$("#background-url-carta-input").on("input", function(){
+    $("#background-carta").attr("src", $(this).val());
+});
+$("#icon-url-carta-input").on("input", function(){
+    $("#icon-img").attr("src", $(this).val());
+});
+$("#border-url-carta-input").on("input", function(){
+    $("#top-name-config-img").attr("src", $(this).val());
+});
+$("#descricao-carta-input").on("input", function(){
+    $("#descricao-carta").html($(this).val());
+});
+$("#descricao-carta-input2").on("input", function(){
+    $("#descricao-carta2").html($(this).val());
+});
+$("#atributos-carta-input").on("input", function(){
+    $("#atributos-carta").html($(this).val());
+});
+$("#atributos-carta-input2").on("input", function(){
+    $("#atributos-carta2").html($(this).val());
+});
+$("#textura-carta-input").on('change', function() {
+    $("#carta").removeClass();
+    $("#carta").addClass("carta "+this.value);
+});
+
+$("#cor-carta-input").change(function(){
+    //$("#nome-carta").css('background', $(this).val()); custo-mana-carta
+    $("#carta").css('border', '2px solid '+$(this).val());
+    $("#imagem-carta-container").css('border', '2px solid '+$(this).val());
+    $(".config-descricao-carta").css('border', '2px solid '+$(this).val());
+    $("#background-carta").css('border', '2px solid '+$(this).val());
+    $("#custo-mana-carta").css('background-color', $(this).val());
+});
+$("#cor-texto-carta-input").change(function(){
+    //$("#nome-carta").css('background', $(this).val()); custo-mana-carta
+    $("#nome-carta").css('color', $(this).val());
+    $("#custo-mana-carta").css('color', $(this).val());
+    $("#descricao-carta").css('color', $(this).val());
+    $("#atributos-carta").css('color', $(this).val());
+    $("#descricao-carta2").css('color', $(this).val());
+    $("#atributos-carta2").css('color', $(this).val());
+});
+
+
+//imagem da carta
+$('select').change(function() {
+   // console.log($(this).val())
+    var e= $(this).val();
+    // Configuração do Background da carta 
+
+    if(e=="4"){
+        document.getElementById('upload-backimage-button').style.display='block';
+        document.getElementById('background-url-carta-input').style.display='none';
+        $("#cor-fundo-carta-input").attr("hidden", true);
+    }
+    if(e=="5"){
+        document.getElementById('upload-backimage-button').style.display='none';
+        document.getElementById('background-url-carta-input').style.display='block';
+        $("#cor-fundo-carta-input").attr("hidden", true);
+    }
+    if(e=="6"){
+        document.getElementById('upload-backimage-button').style.display='none';
+        document.getElementById('background-url-carta-input').style.display='none';
+        $("#cor-fundo-carta-input").attr("hidden", true);
+    }
+    if(e=="7"){
+        document.getElementById('upload-backimage-button').style.display='none';
+        document.getElementById('background-url-carta-input').style.display='none';
+        $("#cor-fundo-carta-input").attr("hidden", false);
+    }
+
+    // Configuração da Borda
+
+    if(e=="8"){
+        document.getElementById('upload-borderimg-button').style.display='block';
+        document.getElementById('border-url-carta-input').style.display='none';
+        $("#cor-fundo-carta-input").attr("hidden", true);
+    }
+    if(e=="9"){
+        document.getElementById('upload-borderimg-button').style.display='none';
+        document.getElementById('border-url-carta-input').style.display='block';
+        $("#cor-fundo-carta-input").attr("hidden", true);
+    }
+    if(e=="10"){
+        document.getElementById('upload-borderimg-button').style.display='none';
+        document.getElementById('border-url-carta-input').style.display='none';
+        $("#cor-fundo-carta-input").attr("hidden", true);
+    }
+    if(e=="11"){
+        document.getElementById('upload-borderimg-button').style.display='none';
+        document.getElementById('border-url-carta-input').style.display='none';
+        $("#cor-fundo-carta-input").attr("hidden", true);
+        $("#top-name-config-img").attr('style', "opacity: 0");
+    }
+
+    // Configuração do icone
+    
+    if(e=="12"){
+        document.getElementById('upload-iconimg-button').style.display='block';
+        document.getElementById('icon-url-carta-input').style.display='none';
+    }
+    if(e=="13"){
+        document.getElementById('upload-iconimg-button').style.display='none';
+        document.getElementById('icon-url-carta-input').style.display='block';
+    }
+    if(e=="14"){
+        document.getElementById('upload-iconimg-button').style.display='none';
+        document.getElementById('icon-url-carta-input').style.display='none';
+        $("#top-name-config-img").attr('style', "opacity: 0");
+    }
+    if(e=="15"){
+        document.getElementById('upload-iconimg-button').style.display='none';
+        document.getElementById('icon-url-carta-input').style.display='none';
+    }
+
+    // 
+
+    if(e=="24"){
+        document.getElementById('button-03').style.display='none';  
+        document.getElementById('button').style.display='block';        
+    }
+    if(e=="25"){
+        document.getElementById('button').style.display='none';  
+        document.getElementById('button-03').style.display='block';        
+    }
+
+    
+});
+
+
+
+
+
+
+    $("#carta").removeClass();
+    $("#carta").addClass("carta "+this.value);
+
+    $("#cor-fundo-carta-input").change(function(){
+         $("#carta").css('background-color', $(this).val());
+         $('#background-carta')
+                        .attr('style', "opacity: 0");
+    });
+
+
+
+
+
+
+
+
+
+        //Upload direto do computador
+        
+        $("#upload-backimage-button").click(function(){
+            $("#selecao-arquivo").trigger('click');
+        });
+        
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+        
+                reader.onload = function (e) {
+                    $('#background-carta')
+                        .attr('style', "opacity: 1");
+                    $('#background-carta')
+                        .attr('src', e.target.result);
+                    $("#cor-fundo-carta-input").change(function(){
+                    $("#carta").css('background-color', 'opacity: 0');
+                    });
+
+                };
+        
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+      
+        $("#upload-img").click(function(){
+            $("#selecao-arquivo-img").trigger('click');
+        });
+        
+        function readURL2(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+        
+                reader.onload = function (e) {
+                    $('#img-carta')
+                        .attr('src', e.target.result);
+                };
+        
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+        $("#upload-borderimg-button").click(function(){
+            $("#selecao-arquivo-bord").trigger('click');
+        });
+        
+        function readURL3(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+        
+                reader.onload = function (e) {
+                    $('#top-name-config-img')
+                        .attr('src', e.target.result);
+                    $("#top-name-config-img").attr('style', "opacity: 1");
+                };
+        
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+        $("#upload-iconimg-button").click(function(){
+            $("#selecao-arquivo-icon").trigger('click');
+        });
+        
+        function readURL4(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+        
+                reader.onload = function (e) {
+                    $('#icon-img')
+                        .attr('src', e.target.result);
+                };
+        
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+
+
+        
+       
